4eeba7f1f1086a52a7789e689d6ef8b4d0b3864f
rvincken <2403211vincken@zuyd.nl>
2024-12-10 13:51:33 +0100

    initial commit

---
+++ dobbel2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace dobbel2;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dobbelsteen dobbel1 = new Dobbelsteen("red", "D20", Enumerable.Range(1, 20).ToList());
+    }
+}
---
+++ dobbel2/class/Dobbelsteen.cs
@@ -0,0 +1,21 @@
+﻿namespace dobbel2;
+
+class Dobbelsteen
+{
+    public string Color;
+    public string Shape;
+    public List<int> Sides;
+
+    public Dobbelsteen(string color, string shape, List<int> sides)
+    {
+        Color = color;
+        Shape = shape;
+        Sides = sides;
+    }
+
+    public virtual void Dobbel()
+    {
+        Random rnd = new Random();
+        Console.WriteLine(rnd.Next(Sides.Min(), Sides.Max() + 1));
+    }
+}
