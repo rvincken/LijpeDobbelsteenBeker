5a1451b2fbb85792629a3b8b095ccec47131eadc
Ballincat-ops <2407066Barendse@zuyd.nl>
2024-12-10 15:21:56 +0100

    werkt het?

--- dobbel2/class/Beker.cs
+++ dobbel2/class/Beker.cs
@@ -1,14 +1,28 @@
+using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 using System.Security.Cryptography;
 
 namespace dobbel2;
 
 public class Beker
 {
-    public List<int> dobbels = new List<int>();
+    public List<Dobbelsteen> Dobbels = new List<Dobbelsteen>{};
 
-    public Beker()
+
+    public Beker(List<Dobbelsteen> dobbelstenen)
     {
-        
+        Dobbels = dobbelstenen; 
     }   
+    public List<int> DoeWorp()
+    {
+        List<int> geworpenDobbels = new List<int>{};
+        foreach (Dobbelsteen dobbel in Dobbels)
+        {
+            geworpenDobbels.Add(dobbel.Dobbel());
+
+        }
+        return geworpenDobbels;
+
+    }
 }
